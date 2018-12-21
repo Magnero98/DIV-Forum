f2e1d2bf5cbd01cc88596f40208bca97fffe942a
Magnero <lin.yansen@gmail.com>
2018-12-21 21:23:29 +0700

    add new Popularity Domain Model

---
+++ app/Domains/DomainModels/Popularity.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12/21/2018
+ * Time: 12:11 PM
+ */
+
+namespace App\Domains\DomainModels;
+
+
+class Popularity
+{
+    protected $userId;
+    protected $goodPopularity;
+    protected $badPopularity;
+}

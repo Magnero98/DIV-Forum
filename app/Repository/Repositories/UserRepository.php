a2f3a650be7602cc203a05f528931ff9cb0b1728
Magnero <lin.yansen@gmail.com>
2018-12-21 10:20:47 +0700

    add UserRepository

---
+++ app/Repository/Repositories/UserRepository.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12/21/2018
+ * Time: 9:07 AM
+ */
+
+namespace App\Repository\Repositories;
+
+use App\Domains\DomainModels\DomainModel;
+use App\User;
+
+class UserRepository implements Repository
+{
+
+    /**
+     * Retrieve all data from Database with pagination default perpage = 10
+     * @author Yansen
+     *
+     * @param Integer $perPage = 10
+     * @return Collection of Repository/DataModels/User
+     */
+    public function all($perPage = 10)
+    {
+        return User::where('id', '!=', 0)
+            ->paginate($perPage);
+    }
+
+    /**
+     * Retrieve data from Database with specified id
+     * @author Yansen
+     *
+     * @return Repository/DataModels/User
+     */
+    public function find($id)
+    {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * Insert new model to Database
+     * @author Yansen
+     *
+     * @param DomainModel $model
+     * @return void
+     */
+    public function create(DomainModel $model)
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * Update data with specified id inside Database with updated model
+     * @author Yansen
+     *
+     * @param DomainModel $model
+     * @param Integer $id
+     * @return void
+     */
+    public function update(DomainModel $model, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * Delete data with specified id inside Database
+     * @author Yansen
+     *
+     * @param Integer $id
+     * @return void
+     */
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+}

e4cf716e9df0ba43b2e5eb53dc3282ded218e49e
Magnero <lin.yansen@gmail.com>
2018-12-22 18:18:18 +0700

    add new PopularityRepository

---
+++ app/Repository/Repositories/PopularityRepository.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12/22/2018
+ * Time: 9:44 AM
+ */
+
+namespace App\Repository\Repositories;
+
+
+use App\Domains\DomainModels\DomainModel;
+
+class PopularityRepository implements Repository
+{
+
+    /**
+     * Retrieve all data from Database with pagination default perpage = 10
+     * @author Yansen
+     *
+     * @param Integer $perPage = 10
+     * @return Collection of Repository/DataModels/UserDomainModel
+     */
+    public function all($perPage = 10)
+    {
+        // TODO: Implement all() method.
+    }
+
+    /**
+     * Retrieve data from Database with specified id
+     * @author Yansen
+     *
+     * @return Repository/DataModels/UserDomainModel
+     */
+    public function find($id)
+    {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * Insert new model to Database
+     * @author Yansen
+     *
+     * @param DomainModel $model
+     * @return Repository/DataModels/UserDomainModel
+     */
+    public function create(DomainModel $model)
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * Update data with specified id inside Database with updated model
+     * @author Yansen
+     *
+     * @param DomainModel $model
+     * @param Integer $id
+     * @return Boolean
+     */
+    public function update(DomainModel $model, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * Delete data with specified id inside Database
+     * @author Yansen
+     *
+     * @param Integer $id
+     * @return Boolean
+     */
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+}

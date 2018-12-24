38615c6a8375f47ecec8206976fa3e6735aaa625
Alvent <alvent@test.com>
2018-12-24 10:55:39 +0700

    Create Forum DataModels

---
+++ app/Repository/DataModels/Forum.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository\DataModels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Forum extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     * @author Alvent
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'user_id',
+    	'category_id',
+    	'forum_status_id',
+    	'title',
+    	'description',
+    ];
+}

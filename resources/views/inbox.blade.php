f49398ab072f6a86dda056720df4032cd0685b9f
Alvent <alvent@test.com>
2018-12-24 13:12:05 +0700

    change date format in inbox view

--- resources/views/inbox.blade.php
+++ resources/views/inbox.blade.php
@@ -13,7 +13,7 @@
         @foreach($messages as $message)
             <tr>
                 <td>{{$message->User::find($message->sender_id)->name}}</td>
-                <td>{{$message->created_at}}</td>
+                <td>{{$message->created_at->format('l, d-M-Y H:i:s')}}</td>
                 <td>{{$message->content}}</td>
                 <td>
                     <form action="{{url('inbox/'.$message->id)}}" method="post">

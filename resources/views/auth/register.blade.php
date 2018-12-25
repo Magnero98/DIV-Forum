2eb43879233c23d2e1fef189f34fc0641b3a2934
Magnero <lin.yansen@gmail.com>
2018-12-25 20:08:31 +0700

    add new register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -137,7 +137,7 @@
                             </div>
                         </div>
 
-                        <div class="checkbox {{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="checkbox {{ $errors->has('agreement') ? ' has-error' : '' }}">
                             <div class="col-md-3"></div>
                             <div class="col-md-9">
                                 <label class="text-center">

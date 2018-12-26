67f0ced37ea589e4cd9ea09a256e3a1ecf214860
Magnero <lin.yansen@gmail.com>
2018-12-26 11:24:54 +0700

    define a relation with user in Role Data Model

--- app/Repository/DataModels/Role.php
+++ app/Repository/DataModels/Role.php
@@ -15,4 +15,15 @@ class Role extends Model
     protected $fillable = [
         'name',
     ];
+
+    /**
+     * Role relation: One Role has many users
+     * @author Yansen
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }

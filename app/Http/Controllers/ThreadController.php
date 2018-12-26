5d6daa9933538f675aea5860d3365d1984a44d8a
Magnero <lin.yansen@gmail.com>
2018-12-26 11:18:19 +0700

    add new ThreadController

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domains\DomainModels\ThreadDomainModel;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        ThreadDomainModel::addThread($request->all());
+
+        return back();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $thread = ThreadDomainModel::findThread($id);
+
+        return view('threads.edit')
+            ->with('thread', $thread);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        ThreadDomainModel::editThread($request->all(), $id);
+
+        return redirect(route('forums.show', ['id' => $request->forum_id]));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        ThreadDomainModel::deleteThread($id);
+
+        return back();
+    }
+}

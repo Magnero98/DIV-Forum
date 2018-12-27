a8a91b604e920818d4801d673df83d4714262783
Magnero <lin.yansen@gmail.com>
2018-12-27 13:59:46 +0700

    add constructor that call validateUserData middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use DateTime;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Validator;
 use App\Domains\DomainModels\UserDomainModel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    /**
+     * UserDomainModel Constructor
+     * @author Yansen
+     */
+    public function __construct()
+    {
+        $this->middleware(
+            'validateUserData',
+            ['only' => ['store', 'update']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -23,29 +32,6 @@ class UserController extends Controller
             ->with('users', $users);
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     * @author Yansen
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-            'phone' => 'required|numeric',
-            'address' => 'required|regex:(street)',
-            'birthday' => 'required|date|before:'
-                . (date_format(new DateTime(), 'Y') - 12)
-                . '-'
-                . date_format(new DateTime(), 'm-d'),
-            'gender' => 'required',
-            'picture' => 'required|image',
-        ]);
-    }
 
     /**
      * Show the form for creating a new resource.
@@ -65,13 +51,8 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = $this->validator($request->all());
-        if($validator->fails())
-            return redirect(route('users.create'))
-                ->withErrors($validator)
-                ->withInput(Input::all());
-
         UserDomainModel::addUser($request->all());
+
         return redirect(route('users.index'));
     }
 
@@ -110,13 +91,8 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $validator = $this->validator($request->all());
-        if($validator->fails())
-            return redirect(route('users.edit', ['id' => $id]))
-                ->withErrors($validator)
-                ->withInput(Input::all());
-
         UserDomainModel::editUser($request->all(), $id);
+
         return redirect(route('users.index'));
     }
 
